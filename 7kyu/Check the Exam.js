a1459e04ed201545ef09e752f42520d7b3b6d08e
Matthew McGrath <rubywep@hotmail.com>
2022-08-05 20:00:12 -0700

    Add daily Codewars challenge(s)

---
+++ 7kyu/Check the Exam.js
@@ -0,0 +1,26 @@
+/*
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+*/
+
+// My solution:
+const checkExam = (correctAnswers, studentAnswers) => {
+  const score = correctAnswers.reduce((acc, curr, index) => {
+    return studentAnswers[index] === '' ? acc : studentAnswers[index] === curr ? Number(acc + 4) : Number(acc - 1);
+  }, 0);
+
+  return score > 0 ? score : 0;
+};
+
+// Short user solution (highly unreadable, though!):
+checkExam = (x, y) => ((x = y.reduce((s, e, i) => s + (e === x[i] ? 4 : e === '' ? 0 : -1), 0)) > 0 ? x : 0);
