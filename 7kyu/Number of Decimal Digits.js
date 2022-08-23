adb2ab01e0fdd5f4c72617411ea7b69d20998a3f
Matthew McGrath <rubywep@hotmail.com>
2022-08-22 17:55:27 -0700

    Add 'Number of Decimal Digits' Codewars solution

---
+++ 7kyu/Number of Decimal Digits.js
@@ -0,0 +1,21 @@
+/*
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+*/
+
+// My solution:
+const digits = (n) => String(n).length;
+
+// Less JavaScript-specific way:
+function digits(n) {
+  let count = 0;
+  if (n >= 0) count = 1;
+
+  while (n / 10 >= 1) {
+    n /= 10;
+    count++;
+  }
+
+  return count;
+}
