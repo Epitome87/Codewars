e26af5481e3e00d89595cad76e39249e966ca80f
Matthew McGrath <rubywep@hotmail.com>
2022-08-24 00:20:07 -0700

    Add 'Help the Fruit Guy' Codewars solution

---
+++ 7kyu/Help the Fruit Guy.js
@@ -0,0 +1,23 @@
+/*
+Our fruit guy has a bag of fruit (represented as an array of strings) where some fruits are rotten. He wants to replace all the rotten pieces of fruit with fresh ones. For example, given ["apple","rottenBanana","apple"] the replaced array should be ["apple","banana","apple"]. Your task is to implement a method that accepts an array of strings containing fruits should returns an array of strings where all the rotten fruits are replaced by good ones.
+
+Notes
+If the array is null/nil/None or empty you should return empty array ([]).
+The rotten fruit name will be in this camelcase (rottenFruit).
+The returned array should be in lowercase.
+*/
+
+// My solution:
+const removeRotten = (bagOfFruits) => {
+  if (!bagOfFruits || bagOfFruits.length === 0) return [];
+  return bagOfFruits
+    .join(' ')
+    .toLowerCase()
+    .replace(/rotten/g, '')
+    .split(' ');
+};
+
+// Top user solution:
+function removeRotten(arr) {
+  return arr ? arr.map((x) => x.replace('rotten', '').toLowerCase()) : [];
+}
