4f6f98aa4427354fc7560f0820bc63c61fb3e566
Matthew McGrath <rubywep@hotmail.com>
2022-08-23 19:30:39 -0700

    Add 'Sum of Triangular Numbers' Codewars solution

---
+++ 7kyu/Sum of Triangular Numbers.js
@@ -0,0 +1,31 @@
+/*
+Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+
+Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.) obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+
+[01]
+02 [03]
+04 05 [06]
+07 08 09 [10]
+11 12 13 14 [15]
+16 17 18 19 20 [21]
+e.g. If 4 is given: 1 + 3 + 6 + 10 = 20.
+
+Triangular Numbers cannot be negative so return 0 if a negative number is given.
+*/
+
+// My solution (Googling how to find nth triangular number):
+const sumTriangularNumbers = (n) => {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += (i * (i + 1)) / 2;
+  }
+
+  return sum;
+};
+
+// Top user solution:
+function sumTriangularNumbers(n) {
+  return n < 0 ? 0 : (n * (n + 1) * (n + 2)) / 6;
+}
