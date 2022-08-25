3c46fd3916f55fddd19351d45d0909e14a67abcd
Matthew McGrath <rubywep@hotmail.com>
2022-08-24 19:22:58 -0700

    Add 'I Love You...' Codewars solution

---
+++ 8kyu/I Love You, A Little, A Lot, Passionately, Not At All.js
@@ -0,0 +1,45 @@
+/*
+Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
+
+I love you
+a little
+a lot
+passionately
+madly
+not at all
+When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+
+Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals, where nb_petals > 0.
+*/
+
+// My solution:
+const howMuchILoveYou = (numPetals) => {
+  switch (numPetals % 6) {
+    case 0:
+      return 'not at all';
+    case 1:
+      return 'I love you';
+    case 2:
+      return 'a little';
+    case 3:
+      return 'a lot';
+    case 4:
+      return 'passionately';
+    case 5:
+      return 'madly';
+  }
+};
+
+// Top user solution (and my other solution):
+const phrases = [
+  'I love you',
+  'a little',
+  'a lot',
+  'passionately',
+  'madly',
+  'not at all',
+];
+
+function howMuchILoveYou(n) {
+  return phrases[(n - 1) % phrases.length];
+}
