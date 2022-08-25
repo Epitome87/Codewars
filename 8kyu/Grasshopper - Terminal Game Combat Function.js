12674dd504384274713d0a6a581ea835fcdba278
Matthew McGrath <rubywep@hotmail.com>
2022-08-25 14:24:06 -0700

    Add 'Grasshopper - Terminal Game...' solution

---
+++ 8kyu/Grasshopper - Terminal Game Combat Function.js
@@ -0,0 +1,6 @@
+/*
+Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+*/
+
+// My solution:
+const combat = (health, damage) => Math.max(0, health - damage);
