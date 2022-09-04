0417a1c6021542be758554ce53b917631d456c54
Matthew McGrath <rubywep@hotmail.com>
2022-09-03 17:54:13 -0700

    Add 'Rock Paper Scissors Lines Spock' solution

---
+++ 7kyu/Rock Paper Scissors Lizard Spock.js
@@ -0,0 +1,33 @@
+/*
+In this kata, your task is to implement an extended version of the famous rock-paper-scissors game. The rules are as follows:
+
+Scissors cuts Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+Rock crushes Scissors
+Task:
+Given two values from the above game, return the Player result as "Player 1 Won!", "Player 2 Won!", or "Draw!".
+
+Inputs
+Values will be given as one of "rock", "paper", "scissors", "lizard", "spock".
+
+
+*/
+
+// My solution:
+const victors = {
+  scissors: ['paper', 'lizard'],
+  paper: ['rock', 'spock'],
+  rock: ['lizard', 'scissors'],
+  lizard: ['spock', 'paper'],
+  spock: ['scissors', 'rock'],
+};
+
+const rpsls = (p1, p2) =>
+  victors[p1].includes(p2) ? 'Player 1 Won!' : victors[p2].includes(p1) ? 'Player 2 Won!' : 'Draw!';
