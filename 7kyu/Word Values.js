f4cd74c3524705440593d90c2eb1ca11ea4b6821
Matthew McGrath <rubywep@hotmail.com>
2022-09-07 19:34:14 -0700

    Add 'Word Values' Codewars solution

---
+++ 7kyu/Word Values.js
@@ -0,0 +1,26 @@
+/*
+Given a string "abc" and assuming that each letter in the string has a value equal to its position in the alphabet, our string will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ....z = 26.
+
+You will be given a list of strings and your task will be to return the values of the strings as explained above multiplied by the position of that string in the list. For our purpose, position begins with 1.
+
+nameValue ["abc","abc abc"] should return [6,24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+
+"abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied by 1 while the value at position 2 is multiplied by 2.
+
+Input will only contain lowercase characters and spaces.
+*/
+
+// My solution:
+const wordValue = (arr) =>
+  arr.map(
+    (str, idx) =>
+      str
+        .replace(/\s/g, '')
+        .split('')
+        .reduce((acc, curr) => acc + curr.charCodeAt(0) - 96, 0) *
+      (idx + 1)
+  );
+
+/* Takeaways: 
+1) Remember, the .replace() method on strings does not replace all instances, unless using Regex with the global flag!
+*/
