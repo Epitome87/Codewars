721c5357e4d004042b3bf0a30f09aa1eae46e099
Matthew McGrath <rubywep@hotmail.com>
2022-09-07 18:06:56 -0700

    Add 'Indexed Capitalization' Codewars solution

---
+++ 7kyu/Indexed Capitalization.js
@@ -0,0 +1,16 @@
+/*
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
+*/
+
+// My solution:
+const capitalize = (str, arr) =>
+  str
+    .split('')
+    .map((c, idx) => (arr.includes(idx) ? c.toUpperCase() : c))
+    .join('');
