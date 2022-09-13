efa5500656cd550cbebe1641579ed89d698f8f2f
Matthew McGrath <rubywep@hotmail.com>
2022-09-12 21:37:05 -0700

    Add 'Smallest Unused ID' Codewars solution

---
+++ 8kyu/Smallest Unused ID.js
@@ -0,0 +1,25 @@
+/*
+Hey awesome programmer!
+
+You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+Go on and code some pure awesomeness!
+*/
+
+// My solution:
+const nextId = (ids) => {
+  for (let i = 0; i <= ids.length; i++) {
+    if (!ids.includes(i)) return i;
+  }
+};
+
+// Top user solution:
+function nextId(ids) {
+  var x = 0;
+  while (ids.includes(x)) x++;
+  return x;
+}
