1938941907e251eb367308afb11ae4727d1715ad
Matthew McGrath <rubywep@hotmail.com>
2022-09-13 19:53:16 -0700

    Add 'Incorrect Division Method' Codewars solution

---
+++ 8kyu/Incorrect Division Method.js
@@ -0,0 +1,6 @@
+/*
+This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+*/
+
+// My solution:
+const solve = (x, y) => x / y;
