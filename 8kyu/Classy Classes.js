f1361637bf6212b652b96ea427248f77e6125bf1
Matthew McGrath <rubywep@hotmail.com>
2022-09-16 10:07:45 -0700

    Add 'Classy Classes' Codewars solution

---
+++ 8kyu/Classy Classes.js
@@ -0,0 +1,44 @@
+/*
+Classy Classes
+Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+
+Task
+Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+
+Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+*/
+
+// My solution:
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return this.getInfo();
+  }
+
+  getInfo() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
+
+// Can also do this, though the challenge seems to ask for the above:
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get info() {
+    return `${this.name}s age is ${this.age}`;
+  }
+}
+
+// Clever user solution:
+class Person {
+  constructor(name, age) {
+    this.info = `${name}s age is ${age}`;
+  }
+}
