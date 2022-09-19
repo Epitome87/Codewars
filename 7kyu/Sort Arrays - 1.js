0ecfefb5812347bf2494fe470980b70a07c57fe5
Matthew McGrath <rubywep@hotmail.com>
2022-09-18 19:48:32 -0700

    Add 'Sort Array - 1' Codewars solution

---
+++ 7kyu/Sort Arrays - 1.js
@@ -0,0 +1,6 @@
+/*
+Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+*/
+
+// My solution:
+const sortme = (names) => names.sort();
