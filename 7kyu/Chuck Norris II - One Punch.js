ce040fd2af612e2d3133109da55aeb57678475a5
Matthew McGrath <rubywep@hotmail.com>
2022-09-20 22:24:49 -0700

    Add 'Chuck Norris II - One Punch' solution

---
+++ 7kyu/Chuck Norris II - One Punch.js
@@ -0,0 +1,19 @@
+/*
+Chuck Norris is the world's toughest man - he once kicked a horse in the chin. Its descendants today are known as giraffes.
+
+Like his punches, Chuck NEVER needs more than one line of code.
+
+Your task, to please Chuck, is to create a function that chains 4 methods on a SINGLE LINE! You can pass with multiple lines, but CHuck will pity you. Go big or go home. ONE LINE!!
+
+Chuck expects his list of favourite items to be split, sorted, joined AND have any occurrences of the letters 'e' and 'a' removed - why, you ask? Well Nunchuks hasn't got the letters 'a' or 'e' in it has it?? Chuck says shut your mouth... and don't forget the capitals.
+
+If anyone dares to provide Chuck with an empty string, an integer or an array, just return a description of their face once Chuck finds out: 'Broken!'
+
+Go, go go!!!
+*/
+
+// My solution:
+const onePunch = (items) =>
+  typeof items !== 'string' || items.length < 1
+    ? 'Broken!'
+    : items.replace(/[ae]/gi, '').split(' ').sort().join(' ');
