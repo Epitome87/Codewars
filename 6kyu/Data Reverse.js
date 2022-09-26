b50f1d385a7a991e907042e120ca49599463bc23
Matthew McGrath <rubywep@hotmail.com>
2022-09-25 22:11:37 -0700

    Add 'Data Reverse' Codewars solution

---
+++ 6kyu/Data Reverse.js
@@ -0,0 +1,41 @@
+/*
+A stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+Note: In the C and NASM languages you are given the third parameter which is the number of segment blocks.
+ */
+
+// My solution:
+const dataReverse = (data) => {
+  const arr = [];
+
+  while (data.length) {
+    arr.push(...data.splice(-8));
+  }
+
+  return arr;
+};
+
+// Top user solution:
+const dataReverse = (data) => {
+  const bytes = [];
+  for (let i = 0; i < data.length; i += 8) {
+    bytes.unshift(...data.slice(i, i + 8));
+  }
+  return bytes;
+};
+
+// Clever user solution:
+const dataReverse = (a) => a.join``.match(/\d{8}/g).reverse().join``.split``.map(Number);
