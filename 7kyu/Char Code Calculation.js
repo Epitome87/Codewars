133ebfa9867fd88dc9e2d00a690672e074e8dfea
Matthew McGrath <rubywep@hotmail.com>
2022-10-11 21:18:49 -0700

    Add 'Char Code Calculation' Codewars solution

---
+++ 7kyu/Char Code Calculation.js
@@ -0,0 +1,34 @@
+/*
+Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+
+'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+
+total1 = 656667
+              ^
+total2 = 656661
+              ^
+Then return the difference between the sum of the digits in total1 and total2:
+
+  (6 + 5 + 6 + 6 + 6 + 7)
+- (6 + 5 + 6 + 6 + 6 + 1)
+-------------------------
+                       6
+*/
+
+// My solution:
+const calc = (x) => {
+  const total1 = x
+    .split('')
+    .map((c) => c.charCodeAt(0))
+    .join('');
+  const total2 = total1.replace(/[7]/g, 1);
+  return (
+    total1.split('').reduce((acc, curr) => acc + +curr, 0) -
+    total2.split('').reduce((acc, curr) => acc + +curr, 0)
+  );
+};
+
+// Clever user solution:
+const calc = (x) =>
+  (x.replace(/./g, (x) => x.charCodeAt()).match(/7/g) || []).length * 6;
