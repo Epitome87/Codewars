0379a4429c78382d2b57f5137ed8a787403eb418
Matthew McGrath <rubywep@hotmail.com>
2022-10-16 20:21:53 -0700

    Add 'Evens and Odds' Codewars solution

---
+++ 7kyu/Evens and Odds.js
@@ -0,0 +1,13 @@
+/*
+This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+*/
+
+// My solution:
+const evensAndOdds = (num) => (num % 2 ? num.toString(16) : num.toString(2));
+
+// Top user solution:
+const evensAndOdds = (n) => n.toString(n % 2 ? 16 : 2);
