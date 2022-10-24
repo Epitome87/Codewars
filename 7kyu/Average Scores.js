3815bb03046d98c76e8a2b24c5a1ab538beeae8f
Matthew McGrath <rubywep@hotmail.com>
2022-10-23 20:15:00 -0700

    Add 'Average Scores' Codewars solution

---
+++ 7kyu/Average Scores.js
@@ -0,0 +1,6 @@
+/*
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+*/
+
+// My solution:
+const average = (scores) => Math.round(scores.reduce((sum, curr) => sum + curr, 0) / scores.length);
