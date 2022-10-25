842f42bcb307ca2b8d6bab06a5eedc8cb55c639f
Matthew McGrath <rubywep@hotmail.com>
2022-10-24 17:07:41 -0700

    Add 'Area of an Arrow' Codewars solution

---
+++ 7kyu/Area of an Arrow.js
@@ -0,0 +1,13 @@
+/*
+Area of an arrow
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+arrow
+
+a and b are integers and > 0
+
+Write a function which returns the area of the arrow.
+*/
+
+// My solution:
+const arrowArea = (a, b) => (a * b) / 4;
