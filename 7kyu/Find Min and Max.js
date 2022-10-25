1d5a659660031dd3f2988df468ec82dfd9fec5bd
Matthew McGrath <rubywep@hotmail.com>
2022-10-24 17:47:50 -0700

    Add 'Find Min and Max' Codewars solution

---
+++ 7kyu/Find Min and Max.js
@@ -0,0 +1,6 @@
+/*
+Implement a function that returns the minimal and the maximal value of a list (in this order).
+*/
+
+// My solution:
+const getMinMax = (arr) => [Math.min(...arr), Math.max(...arr)];
