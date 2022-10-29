04f1499627bf426bd51347fb028dd33da677ca08
Matthew McGrath <rubywep@hotmail.com>
2022-10-28 17:47:12 -0700

    Add 'Perimeter Sequence' Codewars solution

---
+++ 7kyu/Perimeter Sequence.js
@@ -0,0 +1,13 @@
+/*
+Perimeter sequence
+The first three stages of a sequence are shown.
+
+blocks
+
+The blocksize is a by a and a ≥ 1.
+
+What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+*/
+
+// My solution:
+const perimeterSequence = (a, n) => a * n * 4;
