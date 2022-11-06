e5ba987238e750813bce15313e3ca0565b850e95
Matthew McGrath <rubywep@hotmail.com>
2022-11-05 18:39:17 -0700

    Add 'Regex Basics - Is It A Letter?' solution

---
+++ 7kyu/Regex Basics - Is It A Letter.js
@@ -0,0 +1,8 @@
+/*
+Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
+*/
+
+// My solution:
+String.prototype.isLetter = function () {
+  return /^[a-z]$/i.test(this);
+};
