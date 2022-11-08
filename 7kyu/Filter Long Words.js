27f8178ed9390dd6ab7468310518ebd23efedcb8
Matthew McGrath <rubywep@hotmail.com>
2022-11-08 15:29:56 -0800

    Add 'Filter Long Words' Codewars solution

---
+++ 7kyu/Filter Long Words.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+*/
+
+// My solution:
+const filterLongWords = (sentence, n) => sentence.split(' ').filter((word) => word.length > n);
