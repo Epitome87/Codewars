e236bb907e067454d2c74bbb2f249126644a9e64
Matthew McGrath <rubywep@hotmail.com>
2022-11-11 18:55:39 -0800

    Add 'Are They Square?' Codewars solution

---
+++ 7kyu/Are They Square.js
@@ -0,0 +1,23 @@
+/*
+Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+Your function should return true if all elements in the array are square numbers and false if not.
+
+An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+Examples:
+
+isSquare([1, 4, 9, 16]) --> true
+
+isSquare([3, 4, 7, 9]) --> false
+
+isSquare([]) --> undefined
+is_square([1, 4, 9, 16]) --> True
+
+is_square([3, 4, 7, 9]) --> False
+
+is_square([]) --> None
+*/
+
+// My solution:
+const isSquare = (arr) => (arr.length ? arr.every((n) => Math.sqrt(n) % 1 === 0) : undefined);
