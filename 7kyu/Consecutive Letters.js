4cdc468176d0ac65411ab0812df3814a7eb03b59
Matthew McGrath <rubywep@hotmail.com>
2022-11-30 17:01:59 -0800

    Add 'Consecutive Letters' JavaScript solution

---
+++ 7kyu/Consecutive Letters.js
@@ -0,0 +1,45 @@
+/*
+In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+
+Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+For example: 
+solve("abc") = True, because it contains a,b,c
+solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+solve("dabc") = True, because it contains a, b, c, d
+solve("abbc") = False, because b does not occur once.
+solve("v") = True
+All inputs will be lowercase letters.
+
+More examples in test cases. Good luck!
+*/
+
+// My original solution:
+const solve = (s) => {
+  const set = [...new Set(s)];
+  if (set.length !== s.length) return false;
+
+  const sorted = [...set].sort();
+
+  for (let i = 1; i < sorted.length; i++) {
+    const charCode = sorted[i].charCodeAt(0);
+    const prevCharCode = sorted[i - 1].charCodeAt(0);
+    if (charCode !== prevCharCode - 1 && charCode !== prevCharCode + 1)
+      return false;
+  }
+
+  return true;
+};
+
+// Top user solution:
+function solve(s) {
+  return 'abcdefghijklmnopqrstuvwxyz'.includes([...s].sort().join(''));
+}
+
+// Another good user solution:
+function solve(s) {
+  const a = [...s].sort();
+  return a.every(
+    (v, i) => i === 0 || v.charCodeAt(0) - a[i - 1].charCodeAt(0) === 1
+  );
+}
