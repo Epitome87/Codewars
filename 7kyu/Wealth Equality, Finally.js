feea09aa977c777c55d78a15354cbadc0ae8e537
Matthew McGrath <rubywep@hotmail.com>
2022-12-03 17:09:05 -0800

    Add 'Wealth Equality, Finally' JavaScript solution

---
+++ 7kyu/Wealth Equality, Finally.js
@@ -0,0 +1,48 @@
+/*
+The year is 2088 and the Radical Marxist Socialist People's Party (RMSPP) has just seized power in Brazil.
+
+Their first act in power is absolute wealth equality through coercive redistribution.
+
+Create a function that redistributes all wealth equally among all citizens.
+
+Wealth is represented as an array/list where every index is the wealth of a single citizen.
+The function should mutate the input such that every index has the same amount of wealth.
+
+See example:
+
+Input:
+[5, 10, 6]  >>> This represents:
+           # citizen 1 has wealth 5
+           # citizen 2 has wealth 10
+           # citizen 3 has wealth 6
+Should be after the test:
+ [7, 7, 7] >>> wealth has now been equally redistributed
+Info:
+
+MUTATE the input array/list, don't return anything
+Input is guaranteed to hold at least 1 citizen
+Wealth of a citizen will be an integer with minimum equal to 0 (negative wealth is not possible)
+Handling of floating point error will not be tested
+*/
+
+// My solution:
+const redistributeWealth = (wealth) => {
+  const averageWealth = wealth.reduce((sum, curr) => sum + curr, 0) / wealth.length;
+  for (let i = 0; i < wealth.length; i++) {
+    wealth[i] = averageWealth;
+  }
+};
+
+// Top user solution:
+function redistributeWealth(wealth) {
+  wealth = wealth.fill(
+    wealth.reduce(function (a, b) {
+      return a + b;
+    }) / wealth.length
+  );
+}
+
+/* Takeaways:
+1) Remember, most of the built-in array methods do not mutate, which is what this Kata wants us to do.
+2) Also remember, a for-of loop does not mutate either.
+*/
