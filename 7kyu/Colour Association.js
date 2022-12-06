622809208487dc48a34e2f7b0c4ed2bcb6d06988
Matthew McGrath <rubywep@hotmail.com>
2022-12-05 22:33:22 -0800

    Add 'Colour Association' JavaScript solution

---
+++ 7kyu/Colour Association.js
@@ -0,0 +1,15 @@
+/*
+Colour plays an important role in our lifes. Most of us like this colour better then another. User experience specialists believe that certain colours have certain psychological meanings for us.
+
+You are given a 2D array, composed of a colour and its 'common' association in each array element. The function you will write needs to return the colour as 'key' and association as its 'value'.
+
+For example:
+
+var array = [["white", "goodness"], ...] //returns [{white: 'goodness'}, ...]
+*/
+
+// My solution:
+const colourAssociation = (arr) => arr.reduce((acc, curr) => [...acc, { [curr[0]]: curr[1] }], []);
+
+// Top user solution:
+const colourAssociation = (array) => array.map(([colour, association]) => ({ [colour]: association }));
