3e849657a16c641f49ffcb17d5ebcdd8a240c790
Matthew McGrath <rubywep@hotmail.com>
2022-12-24 22:22:50 -0800

    Add 'Kooka-Counter' JavaScript solution

---
+++ 7kyu/Kooka-Counter.js
@@ -0,0 +1,45 @@
+/*
+A family of kookaburras are in my backyard.
+
+I can't see them all, but I can hear them!
+
+How many kookaburras are there?
+
+Hint
+The trick to counting kookaburras is to listen carefully
+
+The males sound like HaHaHa...
+
+The females sound like hahaha...
+
+And they always alternate male/female
+
+Examples
+ha = female => 1
+Ha = male => 1
+Haha = male + female => 2
+haHa = female + male => 2
+hahahahaha = female => 1
+hahahahahaHaHaHa = female + male => 2
+HaHaHahahaHaHa = male + female + male => 3
+^ Kata Note : No validation is necessary; only valid input will be passed :-)
+*/
+
+// My eh solution:
+const kookaCounter = (laughing) => {
+  const split = laughing.split('a');
+  let prev = split[0];
+  let count = 0;
+  for (let i = 1; i < split.length; i++) {
+    if (split[i] !== prev) {
+      count++;
+      prev = split[i];
+    }
+  }
+  return count;
+};
+
+// Top user solution:
+var kookaCounter = function (laughing) {
+  return (laughing.match(/(Ha)+|(ha)+/g) || []).length;
+};
