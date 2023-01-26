007d0ed123127a62da19c487efe7a5f07369f997
Matthew McGrath <rubywep@hotmail.com>
2023-01-25 17:18:32 -0800

    Add 'Exclamation Marks Series #6' JS solution

---
+++ 8kyu/Exclamation Marks Series #6.js
@@ -0,0 +1,27 @@
+/*
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+// My solution:
+const remove = (s, n) => {
+  while (n-- > 0) s = s.replace('!', '');
+  return s;
+};
+
+// Clever recursive user solution:
+function remove(s, n) {
+  return n > 0 ? remove(s.replace('!', ''), n - 1) : s;
+}
