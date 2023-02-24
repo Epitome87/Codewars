62188c0569b36185d64a5fe1e51dfbd02ed78527
Matthew McGrath <rubywep@hotmail.com>
2023-02-24 13:41:53 -0800

    Add 'Broken Counter' JavaScript solution

---
+++ 8kyu/Broken Counter.js
@@ -0,0 +1,60 @@
+/*
+Our counter prototype is broken. Can you spot, what's wrong here?
+
+Original code:
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase() {
+  this.value++;
+};
+
+Counter.prototype.getValue() {
+  return this.value;
+};
+
+Counter.prototype.reset() {
+  this.value = 0;
+};
+*/
+
+// My solution:
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+};
+
+Counter.prototype.getValue = function () {
+  return this.value;
+};
+
+Counter.prototype.reset = function () {
+  this.value = 0;
+};
+
+// Can also use the newer Class syntax:
+class Counter {
+  constructor() {
+    this.value = 0;
+  }
+
+  increase() {
+    this.value++;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  reset() {
+    this.value = 0;
+  }
+}
+
+/* Takeaways:
+1) The starting code was not setting the prototypes equal to a function properly.
+*/
