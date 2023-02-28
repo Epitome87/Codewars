b80be7e82a2a05fb8d873b1a0ad01fb2e21702aa
Matthew McGrath <rubywep@hotmail.com>
2023-02-27 22:08:11 -0800

    Add 'The Office VI - Sabbatical' JS solution

---
+++ 7kyu/The Office VI - Sabbatical.js
@@ -0,0 +1,28 @@
+/*
+Learning to code around your full time job is taking over your life. You realise that in order to make significant steps quickly, it would help to go to a coding bootcamp in London.
+
+Problem is, many of them cost a fortune, and those that don't still involve a significant amount of time off work - who will pay your mortgage?!
+
+To offset this risk, you decide that rather than leaving work totally, you will request a sabbatical so that you can go back to work post bootcamp and be paid while you look for your next role.
+
+You need to approach your boss. Her decision will be based on three parameters:
+
+val = your value to the organisation
+happiness = her happiness level at the time of asking and finally
+The numbers of letters from 'sabbatical' that are present in string s.
+
+Note that if s contains three instances of the letter 'l', that still scores three points, even though there is only one in the word sabbatical.
+
+If the sum of the three parameters (as described above) is > 22, return 'Sabbatical! Boom!', else return 'Back to your desk, boy.'.
+*/
+
+// My solution:
+const sabb = (s, val, happiness) =>
+  val + happiness + [...s.toLowerCase()].reduce((acc, curr) => ('sabbatical'.includes(curr) ? acc + 1 : acc), 0) > 22
+    ? 'Sabbatical! Boom!'
+    : 'Back to your desk, boy.';
+
+// Top user solution:
+function sabb(x, val, happ) {
+  return (x.match(/[sabbatical]/gi) || []).length + val + happ > 22 ? 'Sabbatical! Boom!' : 'Back to your desk, boy.';
+}
