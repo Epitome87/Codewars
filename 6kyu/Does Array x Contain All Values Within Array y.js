fab0fbbdf81d8291624f46b6bff7cff5c356c9f9
Matthew McGrath <rubywep@hotmail.com>
2023-03-22 15:37:01 -0700

    Add 'Does Array x Contain All Values...' solution

---
+++ 6kyu/Does Array x Contain All Values Within Array y.js
@@ -0,0 +1,30 @@
+/*
+We want to extend the array class so that it provides a contains_all? method. This method will always assume that an array is passed in and will return true if all values in the passed in array are present within the current array.
+
+For example:
+
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+items.containsAll([1, 2, 3]);  =>  true
+items.containsAll([1, 5, 13]);  =>  false // because 13 is not in the items array
+*/
+
+// My solution:
+Array.prototype.containsAll = function (a) {
+  return a.every((x) => this.includes(x));
+};
+
+// My solution (using the author's starting code):
+Object.defineProperty(Array.prototype, 'containsAll', {
+  value: function containsAll(a) {
+    return a.every((x) => this.includes(x));
+  },
+});
+
+// Top user solution:
+Object.defineProperty(Array.prototype, 'containsAll', {
+  value: function containsAll(a) {
+    let combine = new Set(this.concat(a));
+
+    return combine.size == this.length;
+  },
+});
