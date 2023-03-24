242fcaff9e6f845bad423781b3ee0c35ad0e0d42
Matthew McGrath <rubywep@hotmail.com>
2023-03-23 21:12:06 -0700

    Add 'Beeramid' JavaScript solution

---
+++ 5kyu/Beeramid.js
@@ -0,0 +1,28 @@
+/*
+Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+
+Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+
+your referral bonus, and
+
+the price of a beer can
+
+For example:
+
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16
+*/
+
+// My solution:
+const beeramid = (bonus, price) => {
+  for (let level = 1; ; level++) {
+    bonus -= price * level * level;
+    if (bonus < 0) return level - 1;
+  }
+};
+
+// One-line user solution:
+const beeramid = (bonus, price, lvl = 1) =>
+  bonus - lvl ** 2 * price < 0 ? --lvl : beeramid(bonus - lvl ** 2 * price, price, ++lvl);
