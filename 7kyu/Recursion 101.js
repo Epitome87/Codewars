86294a0c94146fc27deadc1915a8dcc7ad176615
Matthew McGrath <rubywep@hotmail.com>
2023-04-03 18:18:15 -0700

    Add 'Recursion 101' JavaScript solution

---
+++ 7kyu/Recursion 101.js
@@ -0,0 +1,20 @@
+/*
+In this Kata, you will be given two positive integers a and b and your task will be to apply the following operations:
+
+i) If a = 0 or b = 0, return [a,b]. Otherwise, go to step (ii);
+ii) If a ≥ 2*b, set a = a - 2*b, and repeat step (i). Otherwise, go to step (iii);
+iii) If b ≥ 2*a, set b = b - 2*a, and repeat step (i). Otherwise, return [a,b].
+a and b will both be lower than 10E8.
+
+More examples in tests cases. Good luck!
+
+Please also try Simple time difference
+*/
+
+// My solution:
+const solve = (a, b) => {
+  if (!a || !b) return [a, b];
+  if (a >= 2 * b) return solve(a - 2 * b, b);
+  if (b >= 2 * a) return solve(a, b - 2 * a);
+  return [a, b];
+};
